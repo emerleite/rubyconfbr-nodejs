b77323c4cadb5f83d5e10e90d0049bbc581e80f0
emerson <emerson@corp.globo.com>
2010-09-29 22:40:38 -0300

    Usando porta do env

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function (request, response) {
+var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World no.de\n' + process.env.PORT);
 });
