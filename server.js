5b884caf2e89fb2f931bcfc1f16494a9165fbe40
emerson <emerson@corp.globo.com>
2010-09-29 22:39:56 -0300

    Usando porta do env

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var http = require('http');
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World no.de\n' + process.env.PORT);
-}).listen(8000);
+});
+
+port = process.env.PORT || "8000";
+
+server.listen(Number(port));
 
 console.log('Server running at http://127.0.0.1:8000/');
