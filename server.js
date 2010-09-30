0646e7357d888c8db9306c869b6540c0b58125cf
emerson <emerson@corp.globo.com>
2010-09-29 22:38:28 -0300

    mostranod porta

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World no.de\n');
+  response.end('Hello World no.de\n' + process.env.PORT);
 }).listen(8000);
 
 console.log('Server running at http://127.0.0.1:8000/');
