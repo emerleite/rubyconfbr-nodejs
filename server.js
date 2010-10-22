e6611c19778dcfd5098157a3a8569aaca6a3beb6
Emerson Macedo <emerson@corp.globo.com>
2010-10-21 23:59:42 -0200

    Cleanup

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ var http = require('http'),
   io = require('socket.io'),
   sys = require('sys'),
   server = http.createServer(function(req, res){ 
-  
     fs.readFile(__dirname + '/index.html', function(err, data){
-    	if (err) return send404(res);
     	res.writeHead(200, {'Content-Type': 'text/html'})
     	res.write(data, 'utf8');
     	res.end();
@@ -13,7 +11,6 @@ var http = require('http'),
 }); 
 
 port = process.env.PORT || "8000";
-
 server.listen(port);
 
 var socket = io.listen(server, {transports:['websocket', 'htmlfile', 'xhr-multipart', 'xhr-polling']}); 
@@ -22,7 +19,7 @@ socket.on('connection', function(client){
   client.on('message', function(data) {
     if (data == 'disconnect') {
       client.send('disconnected !!!');
-      client.connection.close();
+      client.connection.end();
     } else {
       client.send('received your message: ' + data);
     }
