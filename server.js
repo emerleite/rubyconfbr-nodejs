a9d25678050ebc8579a1d0ac109de22b687f64b8
Emerson Macedo <emerson@corp.globo.com>
2010-10-21 23:35:32 -0200

    Testing socket.io

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-var http = require('http');
+var http = require('http'),
+  fs = require('fs'),
+  io = require('socket.io'),
+  sys = require('sys'),
+  server = http.createServer(function(req, res){ 
+  
+    fs.readFile(__dirname + '/index.html', function(err, data){
+    	if (err) return send404(res);
+    	res.writeHead(200, {'Content-Type': 'text/html'})
+    	res.write(data, 'utf8');
+    	res.end();
+    });
+}); 
 
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World no.de\n' + process.env.PORT);
+port = process.env.PORT || "8000";
+
+server.listen(port);
+
+var socket = io.listen(server, {transports:['websocket', 'htmlfile', 'xhr-multipart', 'xhr-polling']}); 
+socket.on('connection', function(client){ 
+  console.log('Num Clients: ' + socket.clients.length);
+  client.on('message', function(data) {
+    if (data == 'disconnect') {
+      client.send('disconnected !!!');
+      client.connection.close();
+    } else {
+      client.send('received your message: ' + data);
+    }
+  }); 
+  client.on('disconnect', function(){ console.log('disconnected'); }); 
 });
-
-//port = process.env.PORT || "8000";
-//server.listen(Number(port));
-server.listen(Number(process.env.PORT));
