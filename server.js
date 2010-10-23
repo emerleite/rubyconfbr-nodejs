702045077ef2034ae57eb11a628a68b0c6b402bb
Emerson Macedo <emerson@corp.globo.com>
2010-10-23 17:57:41 -0200

    Limpando o que não é mais necessário

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-// var http = require('http'),
-//   fs = require('fs'),
-//   io = require('socket.io'),
-//   sys = require('sys'),
-//   server = http.createServer(function(req, res){ 
-//     fs.readFile(__dirname + '/index.html', function(err, data){
-//      res.writeHead(200, {'Content-Type': 'text/html'})
-//      res.write(data, 'utf8');
-//      res.end();
-//     });
-// }); 
-
-
 var express = require('express'),
     faye = require('faye');
 
@@ -26,9 +13,6 @@ app.configure(function () {
 });
 
 app.post('/', function(req, res) {
-   //console.log(req.body.texto);
-   //res.send("enviado com sucesso");
-   //res.send(200);
    bayeux.getClient().publish('/temporeal', req.body.texto);
    res.send(200);
 });
@@ -36,17 +20,3 @@ app.post('/', function(req, res) {
 port = process.env.PORT || "8000";
 bayeux.attach(app);
 app.listen(port);
-
-// var socket = io.listen(server, {transports:['websocket', 'htmlfile', 'xhr-multipart', 'xhr-polling']}); 
-// socket.on('connection', function(client){ 
-//   console.log('Num Clients: ' + socket.clients.length);
-//   client.on('message', function(data) {
-//     if (data == 'disconnect') {
-//       client.send('disconnected !!!');
-//       client.connection.end();
-//     } else {
-//       client.send('received your message: ' + data);
-//     }
-//   }); 
-//   client.on('disconnect', function(){ console.log('disconnected'); }); 
-// });
