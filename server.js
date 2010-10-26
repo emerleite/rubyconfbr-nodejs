10185dc78ac5c66c73c07c3fe54fbc07115d2621
emerleite <emerleite@gmail.com>
2010-10-25 22:56:22 -0200

    generate server.js

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-(function() {
-  var app, bayeux, express, faye, port;
-  express = require('express');
-  faye = require('faye');
-  bayeux = new faye.NodeAdapter({
-    mount: '/faye',
-    timeout: 45
-  });
-  app = express.createServer();
-  app.configure(function() {
-    app.use(express.logger());
-    app.use(express.bodyDecoder());
-    return app.use(express.staticProvider("" + (__dirname) + "/public"));
-  });
-  app.post('/lance', function(req, res) {
-    if (req.param('token') !== 'abc') {
-      res.send(403);
-      return null;
-    }
-    bayeux.getClient().publish('/temporeal', {
-      time: req.body.time,
-      texto: req.body.texto
-    });
-    return res.send(200);
-  });
-  port = process.env.PORT || "8000";
-  bayeux.attach(app);
-  app.listen(port);
-}).call(this);
+var express = require('express'),
+    faye = require('faye');
+
+var bayeux = new faye.NodeAdapter({
+  mount:    '/faye',
+  timeout:  45
+});
+    
+var app = express.createServer();
+app.configure(function () {
+   app.use(express.logger());
+   app.use(express.bodyDecoder());
+   app.use(express.staticProvider(__dirname + '/public'));
+});
+
+app.post('/lance', function(req, res) {
+  if (req.param('token') != 'abc') { res.send(403); return; }
+  bayeux.getClient().publish('/temporeal', {texto: req.body.texto});
+  res.send(200);
+});
+
+port = process.env.PORT || "8000";
+bayeux.attach(app);
+app.listen(port);
